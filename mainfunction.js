06879a8f9af080c9d5550c2f41aebec6002fc799
nctkorii <135648547+nctkorii@users.noreply.github.com>
2024-11-07 11:31:11 -0500

    Delete mainfunction.js

--- mainfunction.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-
-ctx.beginPath();
-ctx.arc(95, 150, 40, 0, 2 * Math.PI);
-ctx.fillStyle = "lightblue";
-ctx.fill();
-
